50dfbf3ee18b32398695feb21d18b0a1c6c12f33
Vasileios <vtvasileiadis@gmail.com>
2024-03-16 11:38:56 +0200

    reusable anchor tag

---
+++ src/components/shared/Anchor.vue
@@ -0,0 +1,28 @@
+<template>
+  <a
+    :href="link"
+    class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    {{ text }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: "Anchor",
+
+  props: {
+    link: {
+      type: String,
+      default: "",
+    },
+
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,55 +1,39 @@
 <template>
-  <footer class="max-w-md pb-16 text-sm text-slate-500 sm:pb-0">
+  <footer class="max-w-lg pb-16 text-sm text-slate-500 sm:pb-0">
     <p>
       Created in
-      <a
-        href="https://code.visualstudio.com/"
-        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        Visual Studio Code
-      </a>
+
+      <Anchor
+        :link="'https://code.visualstudio.com/'"
+        :text="'Visual Studio Code'"
+      />
+
       by yours truly. Built with
-      <a
-        href="https://vuejs.org/"
-        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        Vue.js
-      </a>
+
+      <Anchor :link="'https://vuejs.org/'" :text="'Vue.js'" />
+
       and
-      <a
-        href="https://tailwindcss.com/"
-        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        Tailwind CSS
-      </a>
+
+      <Anchor :link="'https://tailwindcss.com/'" :text="'Tailwind CSS'" />
+
       , deployed with
-      <a
-        href=""
-        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        .........
-      </a>
+
+      <Anchor :link="''" :text="'Web Hosting'" />
+
       . Inspiration and design by
-      <a
-        href="https://brittanychiang.com/"
-        class="font-medium text-slate-400 hover:text-teal-300 focus-visible:text-teal-300"
-        target="_blank"
-        rel="noreferrer noopener"
-      >
-        Brittany Chiang
-      </a>
+
+      <Anchor :link="'https://brittanychiang.com/'" :text="'Brittany Chiang'" />
     </p>
   </footer>
 </template>
 
 <script>
-export default {};
+import Anchor from "./Anchor.vue";
+export default {
+  name: "Footer",
+
+  components: {
+    Anchor,
+  },
+};
 </script>
