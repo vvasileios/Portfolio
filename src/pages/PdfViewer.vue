5429ddca81c7610607578480a063131926b92c32
Vasileios <vtvasileiadis@gmail.com>
2024-03-30 14:01:05 +0200

    start building pdf viewer

---
+++ src/pages/PdfViewer.vue
@@ -0,0 +1,24 @@
+<template>
+  <iframe :src="viewerUrl"></iframe>
+</template>
+
+<script>
+export default {
+  name: "PdfViewer",
+
+  data() {
+    return {
+      pdfUrl: "/src/assets/dummy.pdf",
+      baseViewerUrl: "https://docs.google.com/viewer?url=",
+    };
+  },
+
+  computed: {
+    viewerUrl() {
+      return `${this.baseViewerUrl}${encodeURIComponent(
+        this.pdfUrl
+      )}&embedded=true`;
+    },
+  },
+};
+</script>
