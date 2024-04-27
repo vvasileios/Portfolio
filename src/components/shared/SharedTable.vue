9b845dab9f3723333ee377d1a176f166c4daed2c
Vasileios <vtvasileiadis@gmail.com>
2024-04-27 18:36:18 +0300

    create base table comp

--- src/components/shared/SharedTable.vue
+++ src/components/shared/SharedTable.vue
@@ -1,30 +1,53 @@
 <script setup></script>
 
 <template>
-  <div>
-    <table>
-      <thead>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+  <div
+    class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0"
+  >
+    <div class="lg:py-24">
+      <h1 class="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+        Projects
+      </h1>
+      <table class="mt-12 w-full border-collapse text-left">
+        <thead
+          class="sticky top-0 z-10 border-b border-slate-300/10 bg-slate-900/75 px-6 py-5 backdrop-blur"
+        >
+          <tr>
+            <th
+              class="hidden py-4 pr-8 text-sm font-semibold text-slate-200 sm:table-cell"
+            >
+              Project
+            </th>
+            <th
+              class="hidden py-4 pr-8 text-sm font-semibold text-slate-200 lg:table-cell"
+            >
+              Smth
+            </th>
+            <th
+              class="hidden py-4 pr-8 text-sm font-semibold text-slate-200 lg:table-cell"
+            >
+              Build with
+            </th>
+            <th class="py-4 pr-8 text-sm font-semibold text-slate-200">Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border-b border-slate-300/10 last:border-none">
+            <td
+              class="hidden py-4 pr-4 align-top font-semibold leading-snug text-slate-200 sm:table-cell"
+            >
+              hahahahaahah
+            </td>
+            <td class="hidden py-4 pr-4 align-top text-sm lg:table-cell">
+              hahahahahahaha
+            </td>
+            <td class="hidden py-4 pr-4 align-top text-sm lg:table-cell">
+              hahahahahaha
+            </td>
+            <td class="py-4 pr-4 align-top text-sm">hahahahahaha</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
