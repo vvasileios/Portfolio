ccdf237df6c222ce993a8681a0c5bc9e72e77a1b
Vasileios <vtvasileiadis@gmail.com>
2024-05-04 17:05:10 +0300

    minor content change/ not final

--- src/content/PortfolioContent.js
+++ src/content/PortfolioContent.js
@@ -15,7 +15,8 @@ export const experienceContent = {
       title: "Software Developer ·",
       company: "StellarBlue",
       link: "https://stellarblue.eu/",
-      description: "",
+      description:
+        "Developed, maintained, and shipped in production, front-end code for the solutions softwares energy sector. Collaborated in an Agile environment, adapting efficiently to evolving project requirements, contributing to the timely delivery of high-quality solutions.",
       techStack: ["JavaScript", "Vue", "Bootstrap", "C#", "Git", "HTML", "CSS"],
     },
     {
@@ -26,7 +27,15 @@ export const experienceContent = {
       link: "https://contractuo.com/",
       description:
         "Developed, maintained, and shipped in production, front-end code for the document management system using the Vue.js ecosystem. Applied OOP principles and the JavaScript Selection API to engineer an advanced application editor. Collaborated in an Agile environment, adapting efficiently to evolving project requirements, contributing to the timely delivery of high-quality solutions.",
-      techStack: ["JavaScript", "Vue", "TailwindCSS", "Git", "HTML", "CSS"],
+      techStack: [
+        "JavaScript",
+        "Vue",
+        "TailwindCSS",
+        "Git",
+        "HTML",
+        "CSS",
+        "Figma",
+      ],
     },
   ],
 };
