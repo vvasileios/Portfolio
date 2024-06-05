edb4de910b092074a6f332459dc1231af10be36a
Vasileios <vtvasileiadis@gmail.com>
2024-06-05 22:39:53 +0300

    add event to resize and change color on torch at smaller screens

--- src/components/shared/TorchMouse.vue
+++ src/components/shared/TorchMouse.vue
@@ -3,14 +3,21 @@ import { ref, computed, onMounted, onBeforeUnmount } from "vue";
 
 const mouseX = ref(0);
 const mouseY = ref(0);
+const windowWidth = ref(window.innerWidth);
 
 onMounted(() => {
   window.addEventListener("mousemove", handleMouseMove);
+  window.addEventListener("resize", handleResize);
 });
 
 onBeforeUnmount(() => {
   window.removeEventListener("mousemove", handleMouseMove);
+  window.removeEventListener("resize", handleResize);
 });
+
+const handleResize = () => {
+  windowWidth.value = window.innerWidth;
+};
 
 const handleMouseMove = (event) => {
   mouseX.value = event.pageX;
@@ -18,12 +25,18 @@ const handleMouseMove = (event) => {
 };
 
 const torchStyle = computed(() => {
+  const transparency = windowWidth.value <= 1500 ? "50%" : "80%";
+  const color =
+    windowWidth.value <= 1500
+      ? "rgba(29, 78, 216, 0.3)"
+      : "rgba(29, 78, 216, 0.2)";
+
   return {
     top: 0,
     left: 0,
     width: "100%",
     height: "100%",
-    background: `radial-gradient(circle 600px at ${mouseX.value}px ${mouseY.value}px, rgba(29, 78, 216, 0.15), transparent 80%)`,
+    background: `radial-gradient(circle 600px at ${mouseX.value}px ${mouseY.value}px, ${color}, transparent ${transparency})`,
   };
 });
 </script>
